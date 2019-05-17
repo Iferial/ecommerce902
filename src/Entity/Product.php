6c77af39a33b8bd2e44c202f29908a091b595852
YevgeniyPivnyak <shkoolnik@gmail.com>
2019-05-17 21:15:17 +0300

    frontend

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -229,7 +229,7 @@ class Product
         return $this->imageName;
     }
 
-    public function setImageName(string $imageName): self
+    public function setImageName(?string $imageName): self
     {
         $this->imageName = $imageName;
 
