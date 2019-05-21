fe7ee7da1b89f8a6b548032f84ba1d88faba1f55
Iferial <santissen@gmail.com>
2019-05-21 18:37:47 +0300

    cart changes

--- src/Service/OrdersService.php
+++ src/Service/OrdersService.php
@@ -70,4 +70,11 @@ class OrdersService
 		return $order;
 	}
 
+	public function setCount(OrderItem $orderItem, int $count): Order
+    {
+        $orderItem->setCount($count);
+        $this->entityManager->flush();
+
+        return $orderItem->getOrder();
+    }
 }
